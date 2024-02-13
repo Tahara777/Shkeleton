25bc6a039b9125989278c9f6d11012b5c3eecd81
Tahara <tahara1644554t@gmail.com>
2024-02-13 22:52:49 +0900

    marge

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,136 @@
+
 #include <M5Core2.h>
-#include "MFRC522_I2C.h" // MFRC522 RFIDリーダー用のI2Cライブラリをインクルード
-#include <SD.h>
-#include <M5GFX.h>// M5GFXライブラリをインクルード
+#include "MFRC522_I2C.h"
+#include <Adafruit_NeoPixel.h>
+#define PIN 26 
+#define NUMPIXELS 21 //LEDの数を指定
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //800kHzでNeoPixelを駆動
 
-M5GFX display;
-M5Canvas Sprite(&display);
-M5Canvas BaseSprite(&Sprite);
-M5Canvas MaskSprite(&Sprite);
+/**RFID**/
+MFRC522_I2C mfrc522(0x28, -1); // Create MFRC522 instance.
+int nocard = 0;
+void uid_display_proc();
 
-MFRC522 mfrc522(0x28); // MFRC522のインスタンスを作成
+/**LED**/
+#define Fingertip2WristMode = 1
+#define Wrist2FingertipMode = 2
+#define ChargeMode = 3
+void Fingertip2Wrist(int cntP, int cntL);
 
-int nocard = 10;
+int mode = 1;
+int cntL = 0;//光演出のカウント
+int cntP = 0;//LEDの場所のカウント
 
-void MaskReveal() {
-  int boxWidth = 20;  // 箱の幅
-  int boxHeight = 20;  // 箱の高さ
+//LED番号再割り当て
+int thumbLine[5] = {2,1,0};
+int indexFingerLine[5] = {7,6,5,4,3};
+int middleFingerLine[5] = {8,9,10,11,12};
+int ringFingerLine[5] = {17,16,15,14,13};
+int littleFingerLine[5] = {18,19,20};
 
-  for(int i = 0; i < 100; i++){
-    // ランダムな位置を計算
-    int x = random(display.width() - boxWidth);
-    int y = random(display.height() - boxHeight);
-    MaskSprite.fillRect(x, y, boxWidth, boxHeight, TFT_WHITE);
-    BaseSprite.pushSprite(0,0);
-    MaskSprite.pushSprite(0,0,TFT_WHITE);
-    Sprite.pushSprite(0, 0);
-  }
-    MaskSprite.fillRect(0, 0, display.width(), display.height(), TFT_WHITE);
-    BaseSprite.pushSprite(0,0);
-    MaskSprite.pushSprite(0,0,TFT_WHITE);
-    Sprite.pushSprite(0, 0);
-    delay(1000); // 小さな遅延を挟む（必要に応じて調整）
+bool isCard = false;
+
+
+
+// ---------------------------------------------------------------
+void setup() {
+    pixels.begin(); //NeoPixelの初期化
+    pixels.clear(); // NeoPixelのリセット
+
+	M5.begin();
+	//M5.lcd.setTextSize(2);
+	//M5.Lcd.println("MFRC522 Test Jun/8/2022");
+	Wire.begin();
+
+	mfrc522.PCD_Init();
+	//M5.Lcd.println("Please put the card\n\nUID:");
 }
 
-
-void setup()
-{
-  M5.begin();
-  display.begin();
-  display.fillScreen(TFT_BLACK);
-
-  Wire.begin(); // I2C通信の初期化
-  mfrc522.PCD_Init(); // MFRC522モジュールの初期化
-
-  // SDカードの初期化
-  if (!SD.begin()) {
-    Serial.println("SDカードの初期化に失敗しました");
-    return;
-  }
-
-
-  // canvasのサイズをディスプレイサイズに設定
-  Sprite.createSprite(display.width(), display.height());
-  BaseSprite.createSprite(display.width(), display.height());
-  MaskSprite.createSprite(display.width(), display.height());
-
-  // SDカードからディレクトリ直下の画像を読み込んでsprite1に描画
-  if (!BaseSprite.drawJpgFile(SD, "/D_K.jpg")) {
-    Serial.println("画像の読み込みに失敗しました");
-    return;
-  }
-  MaskSprite.fillSprite(TFT_BLACK);
-  BaseSprite.pushSprite(0,0);
-  MaskSprite.pushSprite(0,0);
-
-  // canvasをディスプレイに表示
-  Sprite.pushSprite(0, 0);
-}
-
-
+// ---------------------------------------------------------------
 void loop() {
-    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
-        nocard ++;
-    }
-    else{
-        nocard = 0;
-    }  
-    if(nocard < 3){
-        MaskReveal();
-    } else {
+	M5.Lcd.setCursor(50,47);
+	if (!mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {
+	nocard++;
+	if (2 < nocard)
+		{
+		M5.Lcd.println(" No Card       ");
+		Serial.println("*** No Card ***");
+		}
 
-        Serial.println("*** No Card ***");
-        MaskSprite.fillSprite(TFT_BLACK);
-        BaseSprite.pushSprite(0,0);
-        MaskSprite.pushSprite(0,0);
-        Sprite.pushSprite(0, 0);
+	}
+else
+	{
+	nocard = 0;
+	uid_display_proc();
+	isCard =! isCard;
+	}
+
+
+if(isCard){
+	switch(mode){
+      case 1:
+        Fingertip2Wrist(cntP, cntL);
+      break;
+  
+      default:
+        break;
     }
-    //delay(20); // 200ミリ秒待機
+    delay(100); //100ms待機
+
+    cntL +=1;
+    cntP +=1;
+    if(cntP >= 5){
+      cntP = 0;
+    }
+    if(cntL >= 250){
+      cntL = 0;
+    }  
 }
+else{
+	pixels.clear(); // NeoPixelのリセット
+    cntP = 0;
+	cntL = 0;
+}
+
+
+    delay(200);
+}
+
+// ---------------------------------------------------------------
+void uid_display_proc()
+{
+	short aa[] = {1,0,0,0,0,0,0,0,0,0};
+	char buf[60];
+	sprintf(buf,"mfrc522.uid.size = %d",mfrc522.uid.size);
+	Serial.println(buf);
+	for (short it = 0; it < mfrc522.uid.size; it++) {
+		aa[it] = mfrc522.uid.uidByte[it];
+		}
+
+	sprintf(buf, "%02x %02x %02x %02x",aa[0],aa[1],aa[2],aa[3]);
+	M5.Lcd.println(buf);
+	M5.Lcd.println("");
+	Serial.println(buf);
+}
+
+// ---------------------------------------------------------------
+void Fingertip2Wrist(int cntP_a, int cntL_a){
+  pixels.clear(); // NeoPixelのリセット
+  if(cntP_a < 2){
+    pixels.setPixelColor(indexFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(middleFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(ringFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+  }
+  else{
+    pixels.setPixelColor(thumbLine[cntP_a - 2], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(indexFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(middleFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(ringFingerLine[cntP_a], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+    pixels.setPixelColor(littleFingerLine[cntP_a - 2], pixels.Color(cntL_a/2, cntL_a/3, cntL_a)); // i番目の色を設定
+  }
+    pixels.setBrightness(60);// 0~255の範囲で明るさを設定 
+    pixels.show();   //LEDに色を反映
+
+    delay(100);
+}
+// ---------------------------------------------------------------
