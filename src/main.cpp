6de20fa49efadb6d9204ec2695436463d5c838e6
Tahara <tahara1644554t@gmail.com>
2024-02-18 21:56:07 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
-
 #include <M5Core2.h>
 #include "MFRC522_I2C.h"
 #include <Adafruit_NeoPixel.h>
-#define PIN 26 
-#define NUMPIXELS 21 //LEDの数を指定
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //800kHzでNeoPixelを駆動
 
 /**RFID**/
 MFRC522_I2C mfrc522(0x28, -1); // Create MFRC522 instance.
-int nocard = 0;
-void uid_display_proc();
+
 
 /**LED**/
+#define PIN 26 
+#define NUMPIXELS 21 //LEDの数を指定
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //800kHzでNeoPixelを駆動
 #define Fingertip2WristMode = 1
 #define Wrist2FingertipMode = 2
 #define ChargeMode = 3
-void Fingertip2Wrist(int cntP, int cntL);
 
 int mode = 1;
 int cntL = 0;//光演出のカウント
@@ -28,91 +25,114 @@ int middleFingerLine[5] = {8,9,10,11,12};
 int ringFingerLine[5] = {17,16,15,14,13};
 int littleFingerLine[5] = {18,19,20};
 
-bool isCard = false;
+//演出制御
+bool isCard = false;//カード読み取りフラグ
+#define ace "155 43 9 12"
+int CardID = 0;
+float TotalTime = 5;//演出合計時間
+unsigned long startMills = 0;
+unsigned long currentMills = 0;
+unsigned long previousLEDTime= 0;
 
 
+void Fingertip2Wrist(int cntP, int cntL);
+bool isNewCard();
+int identifyCard();
+void LEDcontrol(int B, unsigned long C, unsigned long D);
+void uid_display_proc();
 
 // ---------------------------------------------------------------
 void setup() {
-    pixels.begin(); //NeoPixelの初期化
-    pixels.clear(); // NeoPixelのリセット
+  pixels.begin(); //NeoPixelの初期化
+  pixels.clear(); // NeoPixelのリセット
+
+  Serial.begin(19200); // シリアル通信の開始
 
 	M5.begin();
-	//M5.lcd.setTextSize(2);
-	//M5.Lcd.println("MFRC522 Test Jun/8/2022");
 	Wire.begin();
-
-	mfrc522.PCD_Init();
-	//M5.Lcd.println("Please put the card\n\nUID:");
 }
 
 // ---------------------------------------------------------------
 void loop() {
-	M5.Lcd.setCursor(50,47);
-	if (!mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {
-	nocard++;
-	if (2 < nocard)
-		{
-		M5.Lcd.println(" No Card       ");
-		Serial.println("*** No Card ***");
-		}
+  currentMills = millis(); // 現在のミリ秒を取得
+  //Serial.println(CardID);
+  
+  if(isNewCard()){
+    CardID = identifyCard();
+    isCard = true;
+    //M5.Lcd.println(2);
+    startMills = currentMills; // 正しくグローバル変数を更新
+  }
 
+  if(isCard){
+    LEDcontrol(CardID, startMills, currentMills);
+    if((currentMills - startMills) > 5000){
+      isCard = false; // isCardをbooleanで更新
+    }
+  }
+  else{
+    pixels.clear(); // NeoPixelのリセット
+
+  }
+}
+
+// ---------------------------------------------------------------
+bool isNewCard(){//カード有無を判定
+	if (!mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {//カード読み取り関数
+  return false;
 	}
-else
-	{
-	nocard = 0;
+else{
 	uid_display_proc();
-	isCard =! isCard;
+  return true;
 	}
+}
+// ---------------------------------------------------------------
+int identifyCard(){
+  String strBuf[mfrc522.uid.size];
+  for (byte i = 0; i < mfrc522.uid.size; i++){
+    strBuf[i] = String(mfrc522.uid.uidByte[i], HEX);
 
+    if (strBuf[i].length() == 1){
+      strBuf[i] = "0" + strBuf[i];
+    }
+  }
+  String strUID = strBuf[0] + " " + strBuf[1] + " " + strBuf[2] + " " + strBuf[3];
+  if (strUID.equalsIgnoreCase(ace)){
+    //spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
+    // spriteImage.pushSprite(&lcd, x, y);
+    return 1;
+  }
 
-if(isCard){
-	switch(mode){
+}
+// ---------------------------------------------------------------
+void LEDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime){
+  if(StartTime == CurrentTime){
+    cntL =100;
+    cntP =0;
+  }
+	switch(ID){
       case 1:
+      if((CurrentTime - previousLEDTime) > 100){
         Fingertip2Wrist(cntP, cntL);
-      break;
+        previousLEDTime = CurrentTime;
+        //M5.Lcd.println(previousLEDTime);
+
+          cntL +=10;
+          cntP +=1;
+        if(cntP >= 5){
+          cntP = 0;
+        }
+        if(cntL >= 250){
+          cntL = 250;
+        }
+        break;
   
       default:
         break;
     }
-    delay(100); //100ms待機
 
-    cntL +=1;
-    cntP +=1;
-    if(cntP >= 5){
-      cntP = 0;
-    }
-    if(cntL >= 250){
-      cntL = 0;
     }  
 }
-else{
-	pixels.clear(); // NeoPixelのリセット
-    cntP = 0;
-	cntL = 0;
-}
-
-
-    delay(200);
-}
-
-// ---------------------------------------------------------------
-void uid_display_proc()
-{
-	short aa[] = {1,0,0,0,0,0,0,0,0,0};
-	char buf[60];
-	sprintf(buf,"mfrc522.uid.size = %d",mfrc522.uid.size);
-	Serial.println(buf);
-	for (short it = 0; it < mfrc522.uid.size; it++) {
-		aa[it] = mfrc522.uid.uidByte[it];
-		}
-
-	sprintf(buf, "%02x %02x %02x %02x",aa[0],aa[1],aa[2],aa[3]);
-	M5.Lcd.println(buf);
-	M5.Lcd.println("");
-	Serial.println(buf);
-}
-
 // ---------------------------------------------------------------
 void Fingertip2Wrist(int cntP_a, int cntL_a){
   pixels.clear(); // NeoPixelのリセット
@@ -131,6 +151,22 @@ void Fingertip2Wrist(int cntP_a, int cntL_a){
     pixels.setBrightness(60);// 0~255の範囲で明るさを設定 
     pixels.show();   //LEDに色を反映
 
-    delay(100);
+    //delay(100);
+}
+// ---------------------------------------------------------------
+void uid_display_proc()
+{
+	short aa[] = {1,0,0,0,0,0,0,0,0,0};
+	char buf[60];
+	sprintf(buf,"mfrc522.uid.size = %d",mfrc522.uid.size);
+	Serial.println(buf);
+	for (short it = 0; it < mfrc522.uid.size; it++) {
+		aa[it] = mfrc522.uid.uidByte[it];
+		}
+
+	sprintf(buf, "%02x %02x %02x %02x",aa[0],aa[1],aa[2],aa[3]);
+	M5.Lcd.println(buf);
+	M5.Lcd.println("");
+	Serial.println(buf);
 }
 // ---------------------------------------------------------------
