2515b4242040d3ea603c22978d584b37408662f8
Tahara <tahara1644554t@gmail.com>
2024-02-25 16:13:19 +0900

    画面演出追加

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include "MFRC522_I2C.h"
 #include <Adafruit_NeoPixel.h>
 
+#include <SD.h>
+#include <M5GFX.h>// M5GFXライブラリをインクルード
+M5GFX display;
+M5Canvas Sprite(&display);
+M5Canvas BaseSprite(&Sprite);
+M5Canvas MaskSprite(&Sprite);
+
 /**RFID**/
 MFRC522_I2C mfrc522(0x28, -1); // Create MFRC522 instance.
 
@@ -26,7 +33,7 @@ int littleFingerLine[5] = {18,19,20};
 bool isCard = false;//カード読み取りフラグ
 #define ace "155 43 9 12"
 int CardID = 0;
-float TotalTime = 5;//演出合計時間
+float TotalTime = 5000;//演出合計時間ms
 unsigned long startMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long previousLEDTime= 0;
@@ -38,6 +45,10 @@ int identifyCard();
 void LEDcontrol(int B, unsigned long C, unsigned long D);
 void LCDcontrol(int B, unsigned long C, unsigned long D);
 void uid_display_proc();
+void MaskReveal_Sphere();
+void MaskReveal_Square();
+void ResetLCD();
+void AllLEDon();
 
 // ---------------------------------------------------------------
 void setup() {
@@ -47,7 +58,30 @@ void setup() {
   Serial.begin(19200); // シリアル通信の開始
 
 	M5.begin();
+
+  display.begin();
+  display.fillScreen(TFT_BLACK);
+  mfrc522.PCD_Init(); // MFRC522モジュールの初期化
+
 	Wire.begin();
+
+  // canvasのサイズをディスプレイサイズに設定
+  Sprite.createSprite(display.width(), display.height());
+  BaseSprite.createSprite(display.width(), display.height());
+  MaskSprite.createSprite(display.width(), display.height());
+
+  // SDカードからディレクトリ直下の画像を読み込んでsprite1に描画
+  if (!BaseSprite.drawJpgFile(SD, "/D_K.jpg")) {
+    Serial.println("画像の読み込みに失敗しました");
+    return;
+  }
+  MaskSprite.fillSprite(TFT_BLACK);
+  BaseSprite.pushSprite(0,0);
+  MaskSprite.pushSprite(0,0);
+
+  // canvasをディスプレイに表示
+  Sprite.pushSprite(0, 0);
+
 }
 
 // ---------------------------------------------------------------
@@ -66,8 +100,9 @@ void loop() {
   else{
     LEDcontrol(CardID, startMillis, currentMillis);
     LCDcontrol(CardID, startMillis, currentMillis);
-    if((currentMillis - startMillis) > 5000){
+    if(isNewCard()){//リセット動作が行われた場合
       isCard = false; // isCardをbooleanで更新
+      ResetLCD();
     }
   }
 }
@@ -78,7 +113,7 @@ bool isNewCard(){//カード有無を判定
   return false;
 	}
 else{
-	uid_display_proc();
+	//uid_display_proc();
   return true;
 	}
 }
@@ -106,6 +141,7 @@ void LEDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime){
     ledBrightness =100;
     ledPosition =0;
   }
+  if((CurrentTime - StartTime) < TotalTime){
 	switch(ID){
       case 1:
       if((CurrentTime - previousLEDTime) > 100){//100ms間隔で更新
@@ -121,13 +157,16 @@ void LEDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime){
         if(ledBrightness >= 250){
           ledBrightness = 250;
         }
+      }
         break;
   
       default:
         break;
     }
-
-    }  
+  } 
+  else{
+    AllLEDon();
+  }
 }
 // ---------------------------------------------------------------
 void LCDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime){
@@ -135,30 +174,54 @@ void LCDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime){
     //ledBrightness =100;
     //ledPosition =0;
   }
+  if((CurrentTime - StartTime) < TotalTime){
 	switch(ID){
       case 1:
-      if((CurrentTime - previousLCDTime) > 100){//100ms間隔で更新
-        /*kここに画面演出関数を追加*/
+      if((CurrentTime - previousLCDTime) > 10){//100ms間隔で更新
+          MaskReveal_Sphere();
         previousLCDTime = CurrentTime;
-        //M5.Lcd.println(previousLEDTime);
-
-        /*
-          ledBrightness +=10;
-          ledPosition +=1;
-        if(ledPosition >= 5){
-          ledPosition = 0;
-        }
-        if(ledBrightness >= 250){
-          ledBrightness = 250;
-        }
-        */
+      }
         break;
   
       default:
         break;
-    }
-
     }  
+  }
+  else{
+    MaskSprite.fillRect(0, 0, display.width(), display.height(), TFT_WHITE);
+    BaseSprite.pushSprite(0,0);
+    MaskSprite.pushSprite(0,0,TFT_WHITE);
+    Sprite.pushSprite(0, 0);    
+  }
+}
+// ---------------------------------------------------------------
+void MaskReveal_Sphere() {
+  int boxWidth = 30;  // 箱の幅
+  int boxHeight = 30;  // 箱の高さ
+
+    int x = random(display.width() - boxWidth);
+    int y = random(display.height() - boxHeight);
+
+    MaskSprite.fillCircle( x, y, 30,TFT_WHITE);
+
+    BaseSprite.pushSprite(0,0);
+    MaskSprite.pushSprite(0,0,TFT_WHITE);
+    Sprite.pushSprite(0, 0);
+
+}
+// ---------------------------------------------------------------
+void MaskReveal_Square() {
+  int boxWidth = 30;  // 箱の幅
+  int boxHeight = 30;  // 箱の高さ
+
+    int x = random(display.width() - boxWidth);
+    int y = random(display.height() - boxHeight);
+    
+    MaskSprite.fillRect(x, y, boxWidth, boxHeight, TFT_WHITE);
+    
+    BaseSprite.pushSprite(0,0);
+    MaskSprite.pushSprite(0,0,TFT_WHITE);
+    Sprite.pushSprite(0, 0);
 }
 // ---------------------------------------------------------------
 void Fingertip2Wrist(int ledPosition_a, int ledBrightness_a){
@@ -197,3 +260,41 @@ void uid_display_proc()
 	Serial.println(buf);
 }
 // ---------------------------------------------------------------
+void ResetLCD(){
+  MaskSprite.fillSprite(TFT_BLACK);
+  BaseSprite.pushSprite(0,0);
+  MaskSprite.pushSprite(0,0);
+
+  // canvasをディスプレイに表示
+  Sprite.pushSprite(0, 0);
+}
+// ---------------------------------------------------------------
+
+void AllLEDon(){
+  
+  // 各指のLED配列に対してループを実行
+  /*for(int i=0; i<5; i++){
+    // 2未満のiに対しては、親指と小指を除外
+    if(i < 2){
+      pixels.setPixelColor(indexFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+      pixels.setPixelColor(middleFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+      pixels.setPixelColor(ringFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+    } else {
+      // 親指と小指のLEDも点灯
+      pixels.setPixelColor(thumbLine[i-2], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定    
+      pixels.setPixelColor(indexFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+      pixels.setPixelColor(middleFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+      pixels.setPixelColor(ringFingerLine[i], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+      pixels.setPixelColor(littleFingerLine[i-2], pixels.Color(i*20, i*30, i*40)); // i番目の色を設定
+    }
+  }
+  */
+
+  for(int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255, 255, 255)); // 全てのLEDを白色で点灯
+  }
+
+  pixels.setBrightness(10); // 0~255の範囲で明るさを設定
+  pixels.show(); // LEDに色を反映
+}
+
